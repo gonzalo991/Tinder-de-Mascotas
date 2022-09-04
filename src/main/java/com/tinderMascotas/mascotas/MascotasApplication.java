b800392691deaa1dc529df1c6e639f04c718b731
Gonzalo Araya <gonxaraya991@gmail.com>
2022-09-03 23:13:17 -0300

    Spring security

--- src/main/java/com/tinderMascotas/mascotas/MascotasApplication.java
+++ src/main/java/com/tinderMascotas/mascotas/MascotasApplication.java
@@ -1,13 +1,27 @@
 package com.tinderMascotas.mascotas;
 
+import com.tinderMascotas.mascotas.Servicios.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class MascotasApplication {
+public class MascotasApplication extends WebSecurityConfigurerAdapter {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MascotasApplication.class, args);
-	}
+    @Autowired
+    private UsuarioService usuarioService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(MascotasApplication.class, args);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService((UserDetailsService) usuarioService).passwordEncoder(new BCryptPasswordEncoder());
+    }
 
 }
--- src/main/java/com/tinderMascotas/mascotas/Servicios/UsuarioService.java
+++ src/main/java/com/tinderMascotas/mascotas/Servicios/UsuarioService.java
@@ -5,51 +5,63 @@ import com.tinderMascotas.mascotas.Entity.Usuario;
 import com.tinderMascotas.mascotas.Errors.ErrorService;
 import com.tinderMascotas.mascotas.Respository.UsuarioRepository;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
-public class UsuarioService {
-    
+public class UsuarioService implements UserDetailsService {
+
     @Autowired
     private UsuarioRepository usuarioRepository;
-    
+
     @Autowired
     private FotoService fotoService;
-    
+
     public void registrar(MultipartFile archivo, String nombre, String apellido, String mail, String clave) throws ErrorService, IOException {
-        
+
         validar(nombre, apellido, mail, clave);
-        
+
         Usuario usuario = new Usuario();
         usuario.setNombre(nombre);
         usuario.setApellido(apellido);
         usuario.setMail(mail);
-        usuario.setClave(clave);
+
+        String encClave = new BCryptPasswordEncoder().encode(clave);
+        usuario.setClave(encClave);
         usuario.setAlta(new Date());
-        
+
         Foto foto = fotoService.guardar(archivo);
         usuario.setFoto(foto);
-        
+
         usuarioRepository.save(usuario);
-        
+
     }
-    
+
     public void modificar(MultipartFile archivo, String id, String nombre, String apellido, String mail, String clave) throws ErrorService {
-        
+
         validar(nombre, apellido, mail, clave);
-        
+
         Optional<Usuario> respuesta = usuarioRepository.findById(id);
         if (respuesta.isPresent()) {
             Usuario usuario = respuesta.get();
             usuario.setNombre(nombre);
             usuario.setApellido(apellido);
             usuario.setMail(mail);
-            usuario.setClave(clave);
-            
+            String encClave = new BCryptPasswordEncoder().encode(clave);
+            usuario.setClave(encClave);
+
             String idFoto = null;
             if (usuario.getFoto() != null) {
                 idFoto = usuario.getFoto().getId();
@@ -61,7 +73,7 @@ public class UsuarioService {
             throw new ErrorService("No se encontró el usuario solicitado");
         }
     }
-    
+
     public void deshabilitar(String id) throws ErrorService {
         Optional<Usuario> respuesta = usuarioRepository.findById(id);
         if (respuesta.isPresent()) {
@@ -72,7 +84,7 @@ public class UsuarioService {
             throw new ErrorService("No se encontró el usuario");
         }
     }
-    
+
     public void habilitar(String id) throws ErrorService {
         Optional<Usuario> respuesta = usuarioRepository.findById(id);
         if (respuesta.isPresent()) {
@@ -83,23 +95,50 @@ public class UsuarioService {
             throw new ErrorService("No se encontró el usuario");
         }
     }
-    
+
     public void validar(String nombre, String apellido, String mail, String clave) throws ErrorService {
-        
+
         if (nombre == null || nombre.isEmpty()) {
             throw new ErrorService("El nombre del Usuario no puede ser nulo");
         }
-        
+
         if (apellido == null || apellido.isEmpty()) {
             throw new ErrorService("El mail no puede ser nulo");
         }
-        
+
         if (mail == null || mail.isEmpty()) {
             throw new ErrorService("El e-mail no puede ser nulo");
         }
-        
+
         if (clave == null || clave.isEmpty() || clave.length() <= 6) {
             throw new ErrorService("La clave no puede ser nula o menor a 6 caracteres");
+        }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String mail) throws UsernameNotFoundException {
+
+        Usuario usuario = usuarioRepository.buscarPorMail(mail);
+        if (usuario != null) {
+
+            List<String> permisos = new ArrayList<>();
+            GrantedAuthority p1 = new SimpleGrantedAuthority("MODULO_FOTOS");
+            permisos.add(p1.toString());
+
+            GrantedAuthority p2 = new SimpleGrantedAuthority("MODULO_MASCOTAS");
+            permisos.add(p2.toString());
+
+            GrantedAuthority p3 = new SimpleGrantedAuthority("MODULO_VOTOS");
+            permisos.add(p3.toString());
+
+            User user = new User();
+            user.setName(usuario.getMail());
+            user.setPassword(usuario.getClave());
+            user.setRoles(permisos);
+
+            return (UserDetails) user;
+        } else {
+            return null;
         }
     }
 }
